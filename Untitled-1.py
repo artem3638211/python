015e3c202b7f32f1b2bb5301ca9a07796a9c2493
artem3638211 <artemsamarin1101@gmail.com>
2024-12-25 17:14:38 +0700

    FirstCommit
    мой первый комментарий

---
+++ Untitled-1.py
@@ -0,0 +1,24 @@
+# def dislike(a):
+#      if (type(a)is float or type(a) is int) and a == 6.0:
+#          return "только не 6"
+#      return True
+# print(dislike(6.0))
+# print(dislike(6))
+# print(dislike("6"))
+# print(dislike("хорошо"))
+# print(dislike([6, 6]))
+
+
+a = None
+if a is not None and len(a) > 0:
+    print('OK')
+
+
+a = "нет ошибок"
+if a is not None and len(a) > 0:
+    print("Ok")
+
+
+
+
+
