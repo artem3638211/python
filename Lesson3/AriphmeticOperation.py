ee5942f9e387ee7dd6a2de3bbee3a12c67b6604c
LifeCoreLoop <59015195+LifeCoreLoopExe@users.noreply.github.com>
2025-01-18 15:34:54 +0900

    ChangeForRepArtemkaSweetCake

---
+++ Lesson3/AriphmeticOperation.py
@@ -0,0 +1,7 @@
+print('Введите первое число')
+a = input()
+print('Введите второе число')
+b = input()
+print (str(a), '+' ,str(b), '=' ,int(a)+int(b))
+print (str(a), '-' ,str(b), '=' ,int(a)-int(b))
+print (str(a), '*' ,str(b), '=' ,int(a)*int(b))
