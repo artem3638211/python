372496c4efae2779c936a3bba31406d7dbb5cf07
artem3638211 <artemsamarin1101@gmail.com>
2025-01-25 22:33:38 +0700

    D/z Artem gingerbread

---
+++ Lesson5.py/page4.py
@@ -0,0 +1,11 @@
+a = int(input())
+b = int(input())
+c = int(input())
+if a + b >= c:
+    
+elif a + c >= b:
+
+elif b + c >= a:
+    print("YES")
+else:
+    print("NO")
---
+++ Lesson5.py/page6.py
@@ -0,0 +1,6 @@
+num = int(input("Красивое число: "))
+
+if num % 7 != 0:
+    print("YES")
+else:
+    print("NO")
